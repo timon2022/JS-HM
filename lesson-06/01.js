a1ecb2294208ca281630458a7e73d44bb608bef9
timon6978 <22ar01@mail.ru>
2025-09-01 23:29:08 +0300

    les07

--- lesson-06/01.js
+++ lesson-06/01.js
@@ -29,7 +29,11 @@ function filter(arr,callback)
   let result = []
   for(let i = 0;i<arr.length;i++)
   {
-    if(callback(arr[i],i))result.push(arr[i])
+    if(callback(arr[i],i))
+    {
+      result.push(arr[i])
+    }
+    
   }
   return result
 }
--- lesson-07/01.js
+++ lesson-07/01.js
@@ -16,4 +16,11 @@ const WEB_TECH_IMAGES = [
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/32f74d50-68d0-46aa-b035-7b3a5300d2c1_js-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/c8a1f4a6-1337-4899-bdfd-a8c9c7bb806a_css-magic-logo.jpg',
   'https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/784380b9-6937-42a6-bdfe-869835820234_html-magic-logo.jpg',
-]
+]     
+      let tmp=0
+      let btnPrev=document.getElementById('prev-button').addEventListener('click',()=>{
+        document.querySelector('#web-tech-image').src=WEB_TECH_IMAGES[--tmp<0?tmp=2:tmp]
+      })
+      let btnNext=document.getElementById('next-button').addEventListener('click',()=>{
+        document.querySelector('#web-tech-image').src=WEB_TECH_IMAGES[++tmp>=3?tmp=0:tmp]
+      })
